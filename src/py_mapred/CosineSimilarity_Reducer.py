d77947c3021b7cbd24dc8568659f2deae3afa323
Grg0rry <19084193@imail.sunway.edu.my>
2023-07-22 00:39:57 +0800

    updated py_mapred

---
+++ src/py_mapred/CosineSimilarity_Reducer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+import ast
+
+for line in sys.stdin:
+    last_Tuple = None
+    line = line.strip().split('\t', 1)
+
+    MovieTitle = line[0]
+    vectors = [int(item) for item in ast.literal_eval(line[1])]
+
+    if last_Tuple != None:
+        last_Movie, last_Vector = last_Tuple
+
+        dot_product = sum(x * y for x, y in zip(vectors, last_Vector))
+        magnitude = (sum(x ** 2 for x in vectors) ** 0.5) * (sum(x ** 2 for x in last_Vector) ** 0.5)
+
+        print('%s\t%s' % ((MovieTitle, last_Movie), dot_product/magnitude))
+
+    last_Tuple = (MovieTitle, vectors)
