96c316215c7be5d400a5f777b7b75e9a47d61ae7
Grg0rry <19084193@imail.sunway.edu.my>
2023-07-22 00:26:43 +0800

    updated py_mapred

--- src/py_mapred/MovieNormalization_Mapper.py
+++ src/py_mapred/MovieNormalization_Mapper.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 
 import sys, math
+import ast
 
 for line in sys.stdin:
     line = line.strip().split('\t', 1)
 
     MovieTitle = line[0]
-    vectors = []
-
-    for v in line[1][1:-1].split(','):
-        vectors.append(int(v))
+    vectors = ast.literal_eval(line[1])
 
     length = math.sqrt(sum(v**2 for v in vectors))
     normalized_vector = [v / length for v in vectors]
