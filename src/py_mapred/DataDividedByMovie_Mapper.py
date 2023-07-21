054bd59fe34e77dfaf304a8523f2d0a965f9a174
Grg0rry <19084193@imail.sunway.edu.my>
2023-07-22 02:44:14 +0800

    updated py_mapred

--- src/py_mapred/DataDividedByMovie_Mapper.py
+++ src/py_mapred/DataDividedByMovie_Mapper.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import sys
 
 for line in sys.stdin:
--- src/py_mapred/UserList_Mapper.py
+++ src/py_mapred/UserList_Mapper.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-
 import sys
 
 for line in sys.stdin:
-    line = line.strip().split(",", 5)
+    line = line.strip().split(",")
 
     UserID = int(line[0])
 
