b0bdef749fbc8706610131051e2f17a1adb4fdfb
Grg0rry <19084193@imail.sunway.edu.my>
2023-07-21 23:40:10 +0800

    updated py_mapred

--- src/py_mapred/MoviesVector_Reducer.py
+++ src/py_mapred/MoviesVector_Reducer.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
 
-from operator import itemgetter
 import sys
 
 temp_UserList = []
 MovieRating = {}
+
+# process both mappers
+for line in sys.stdin:
+    line = line.strip().split('\t', 1)
+
+    if line[0].startswith("_$UserList"):
+        temp_UserList.append(line[1])
+    else:
+        UserID, Rating = line[1].split(':')
+        MovieTitle, UserRating = MovieRating.get(MovieTitle, ([],))
+        MovieRating[MovieTitle].append((UserID, Rating))
 
 # process both mappers
 for line in sys.stdin:
