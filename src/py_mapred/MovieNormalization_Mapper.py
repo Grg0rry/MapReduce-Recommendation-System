4df1678c702d0cb0713f63dafea2e4532a07413c
Grg0rry <19084193@imail.sunway.edu.my>
2023-07-22 00:24:22 +0800

    updated py_mapred

--- src/py_mapred/MovieNormalization_Mapper.py
+++ src/py_mapred/MovieNormalization_Mapper.py
@@ -6,8 +6,12 @@ for line in sys.stdin:
     line = line.strip().split('\t', 1)
 
     MovieTitle = line[0]
-    vector = line[1][1:-1].split(',')
-    length = math.sqrt(sum(v**2 for v in vector))
-    normalized_vector = [v / length for v in vector]
+    vectors = []
+
+    for v in line[1][1:-1].split(','):
+        vectors.append(int(v))
+
+    length = math.sqrt(sum(v**2 for v in vectors))
+    normalized_vector = [v / length for v in vectors]
 
     print('%s\t%s' % (MovieTitle, normalized_vector))
