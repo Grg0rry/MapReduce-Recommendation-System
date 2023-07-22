75219f0fd3dab9153acc172391b473a72e21c0fe
Grg0rry <19084193@imail.sunway.edu.my>
2023-07-22 15:00:02 +0800

    updated preprocessing

--- preprocessing/combineMovieTitles.py
+++ preprocessing/combineMovieTitles.py
@@ -14,6 +14,27 @@ if __name__ == '__main__':
     ratings_df = pd.read_csv(args.ratings)
     titles_df = pd.read_csv(args.titles)
 
-    # Compile and Output csv
+    # Merge dataframe
     movies_df = pd.merge(ratings_df, titles_df, how="left", on="MovieID")
+    movies_df = movies_df[['MovieTitle','UserID','Rating']]
+
+    # Remove duplicates
+    movies_df.drop_duplicates()
+
+    # Data slicing
+    f = ['count','mean']
+    df_movie_summary = movies_df.groupby('MovieTitle')['Rating'].agg(f)
+    df_movie_summary.index = df_movie_summary.index.map(int)
+    movie_benchmark = round(df_movie_summary['count'].quantile(0.7),0)
+    drop_movie_list = df_movie_summary[df_movie_summary['count'] < movie_benchmark].index
+
+    df_cust_summary = movies_df.groupby('UserID')['Rating'].agg(f)
+    df_cust_summary.index = df_cust_summary.index.map(int)
+    cust_benchmark = round(df_cust_summary['count'].quantile(0.7),0)
+    drop_cust_list = df_cust_summary[df_cust_summary['count'] < cust_benchmark].index
+    
+    movies_df = movies_df[~movies_df['MovieTitle'].isin(drop_movie_list)]
+    movies_df = movies_df[~movies_df['UserID'].isin(drop_movie_list)]
+
+    # Save output
     movies_df.to_csv(args.output, index=False, header=False)
--- script/preprocessing.sh
+++ script/preprocessing.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 start=$(date +%s)
 
-python3 ./preprocessing/ratingsPreprocessing.py \
+python3 ./preprocessing/RatingsPreprocessing.py \
 -combinedData ./data/combined_data_1.txt,./data/combined_data_2.txt,./data/combined_data_3.txt,./data/combined_data_4.txt \
 -output ./data/cleaned_ratings.csv
 
-python3 ./preprocessing/titlesPreprocessing.py \
+python3 ./preprocessing/TitlesPreprocessing.py \
 -input ./data/movie_titles.csv \
 -output ./data/cleaned_titles.csv
 
-python3 ./preprocessing/combineMovieTitles.py \
+python3 ./preprocessing/CombineMovieTitles.py \
 -ratings ./data/cleaned_ratings.csv \
 -titles ./data/cleaned_titles.csv \
 -output ./data/cleaned_moviesTitles.csv
-
-split -b 1G ./data/cleaned_moviesTitles.csv ./data/cleaned_moviesTitles/
 
 end=$(date +%s)
 total_time=$((end - start))
