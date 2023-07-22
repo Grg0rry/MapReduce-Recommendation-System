27464cebb1f68295ed1fbe13aa9607d97e31d730
Grg0rry <19084193@imail.sunway.edu.my>
2023-07-22 17:49:52 +0800

    updated preprocessing

--- preprocessing/combineMovieTitles.py
+++ preprocessing/combineMovieTitles.py
@@ -18,22 +18,13 @@ if __name__ == '__main__':
     movies_df = pd.merge(ratings_df, titles_df, how="left", on="MovieID")
 
     # Remove duplicates
-    movies_df.drop_duplicates()
+    movies_df.drop_duplicates(inplace=True)
 
     # Data slicing
-    f = ['count','mean']
-    df_movie_summary = movies_df.groupby('MovieID')['Rating'].agg(f)
-    df_movie_summary.index = df_movie_summary.index.map(int)
-    movie_benchmark = round(df_movie_summary['count'].quantile(0.7),0)
-    drop_movie_list = df_movie_summary[df_movie_summary['count'] < movie_benchmark].index
-
-    df_cust_summary = movies_df.groupby('UserID')['Rating'].agg(f)
-    df_cust_summary.index = df_cust_summary.index.map(int)
-    cust_benchmark = round(df_cust_summary['count'].quantile(0.7),0)
-    drop_cust_list = df_cust_summary[df_cust_summary['count'] < cust_benchmark].index
-    
+    movie_quantile = movies_df['MovieID'].quantile(0.7)
+    ovie_counts = movies_df['MovieID'].value_counts()
+    drop_movie_list = movies_df[movies_df < movie_quantile].index
     movies_df = movies_df[~movies_df['MovieID'].isin(drop_movie_list)]
-    movies_df = movies_df[~movies_df['UserID'].isin(drop_movie_list)]
 
     # Save output
     movies_df = movies_df[['MovieTitle','UserID','Rating']]
