62863f6e2365a5f5d7242aeb2bc09425044969d1
Grg0rry <19084193@imail.sunway.edu.my>
2023-07-23 00:49:47 +0800

    updated py_mapred and local_mapred

--- src/py_mapreduce/MoviesVector_Reducer.py
+++ src/py_mapreduce/MoviesVector_Reducer.py
@@ -5,11 +5,16 @@ import sys
 MovieRating = {}
 MovieVector = {}
 UserList = []
+SearchList = []
+num_recommendation = 10
+
 
 for line in sys.stdin:
     line = line.strip().split('\t', 1)
 
     if line[0].startswith("$User_List"):
+        UserList.append(line[1])
+    elif line[0].startswith("$Search_Key"):
         UserList.append(line[1])
     else:
         UserID, Rating = line[1].split(':')
@@ -30,13 +35,22 @@ for MovieTitle, UserRating in MovieRating.items():
     # print('%s\t%s' % (MovieTitle, Vector))
 
 
-movie_pairs = [(MovieTitle, Next_MovieTitle) for MovieTitle in MovieVector for Next_MovieTitle in MovieVector if MovieTitle != Next_MovieTitle]
+for MovieTitle in SearchList:
+    MovieSimilarity = []
 
-for MovieTitle, Next_MovieTitle in movie_pairs:
-    Vector = MovieVector[MovieTitle]
-    Next_Vector = MovieVector[Next_MovieTitle]
+    for Next_MovieTitle in MovieVector:
+        if MovieTitle != Next_MovieTitle:
+            Vector = MovieVector[MovieTitle]
+            Next_Vector = MovieVector[Next_MovieTitle]
+            
+            dot_product = sum(x * y for x, y in zip(Vector, Next_Vector))
+            magnitude = (sum(x ** 2 for x in Vector) ** 0.5) * (sum(x ** 2 for x in Next_Vector) ** 0.5)
+            MovieSimilarity.append((Next_MovieTitle, dot_product/magnitude))
 
-    dot_product = sum(x * y for x, y in zip(Vector, Next_Vector))
-    magnitude = (sum(x ** 2 for x in Vector) ** 0.5) * (sum(x ** 2 for x in Next_Vector) ** 0.5)
-    
-    print('%s\t%s' % ((MovieTitle, Next_MovieTitle), dot_product/magnitude))
+    MovieSimilarity.sort(key=lambda x: x[1], reverse=True)
+
+    print(f'Top {num_recommendation} similar movies to {MovieTitle}')
+    for movie, similarity in MovieSimilarity[:num_recommendation]:
+        print('%s\t%s' % (movie, similarity))
+
+
