498a26fea5b9b7dbdd62ba4790444111ce0db1a7
Grg0rry <19084193@imail.sunway.edu.my>
2023-07-23 16:07:29 +0800

    updated py_mapred and local_mapred

--- src/py_mapreduce/CosineSimilarity_Reducer.py
+++ src/py_mapreduce/CosineSimilarity_Reducer.py
@@ -3,9 +3,11 @@
 import sys
 from itertools import combinations
 import numpy as np
+from collections import defaultdict
 
 Movie_Vector = {}
 Magnitude = {}
+Similarities = defaultdict(list)
 
 for line in sys.stdin:
     line = line.strip().split('\t', 1)
@@ -20,4 +22,10 @@ for (MovieTitle, Vector), (Next_MovieTitle, Next_Vector) in combinations(Movie_V
     dot_product = np.dot(Vector, Next_Vector)
     similarity = dot_product / (Magnitude[MovieTitle] * Magnitude[Next_MovieTitle])
 
-    print('%s\t%s' % ((MovieTitle, Next_MovieTitle), similarity))
+    Similarities[MovieTitle].append((Next_MovieTitle, similarity))
+    Similarities[Next_MovieTitle].append((MovieTitle, similarity))
+
+for movie in Similarities:
+    top_similar_movies = sorted(Similarities[movie], key=lambda x: x[1], reverse=True)[:10]
+    for similar_movie, similarity in top_similar_movies:
+        print('%s\t%s' % ((movie, similar_movie), similarity))
