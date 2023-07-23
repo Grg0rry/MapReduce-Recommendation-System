02f72e22e68c27093a4ec8609038fe8ae0646465
Grg0rry <19084193@imail.sunway.edu.my>
2023-07-23 13:03:29 +0800

    updated py_mapred and local_mapred

--- src/py_mapreduce/CosineSimilarity_Reducer.py
+++ src/py_mapreduce/CosineSimilarity_Reducer.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python3
 
 import sys
+import math
 
 Movie_Vector = {}
+Magnitude = {}
 
 for line in sys.stdin:
     line = line.strip().split('\t', 1)
 
     MovieTitle = line[0]
-    vectors = [int(item.strip()) for item in line[1].split(',')]
-
-    Movie_Vector[MovieTitle] = vectors
+    Vector = [int(item.strip()) for item in line[1].split(',')]
+    Movie_Vector[MovieTitle] = Vector
+    Magnitude[MovieTitle] = math.sqrt(sum(x ** 2 for x in Vector))
 
 for MovieTitle, Vector in Movie_Vector.items():
     for Next_MovieTitle, Next_Vector in Movie_Vector.items():
-        dot_product = sum(x * y for x, y in zip(Vector, Next_Vector))
-        magnitude = (sum(x ** 2 for x in Vector) ** 0.5) * (sum(x ** 2 for x in Next_Vector) ** 0.5)
-        print('%s\t%s' % ((MovieTitle, Next_MovieTitle), dot_product/magnitude))
+        if MovieTitle != Next_MovieTitle:
+            dot_product = sum(x * y for x, y in zip(Vector, Next_Vector))
+            similarity = dot_product / (Magnitude[MovieTitle] * Magnitude[Next_MovieTitle])
+
+            print('%s\t%s' % ((MovieTitle, Next_MovieTitle), similarity))
