cda01988b00f9fabb604fe01dc1cf70ddd593845
Grg0rry <19084193@imail.sunway.edu.my>
2023-07-24 16:24:43 +0800

    just testing

--- src/py_mrjob/tmp.py
+++ src/py_mrjob/tmp.py
@@ -1,5 +1,5 @@
 import subprocess
-cat = subprocess.Popen(["hadoop", "fs", "-cat", "/user/hadoop/results/py_mrjob/job2/part-00000"], stdout=subprocess.PIPE)
+cat = subprocess.Popen(["hadoop", "fs", "-cat", "/user/hadoop/results/py_mapred_streaming/job2/part-00000"], stdout=subprocess.PIPE)
 cnt = 1
 for line in cat.stdout:
     
