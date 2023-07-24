a2717d08db0f0bf14698755393e871e364da1908
Grg0rry <19084193@imail.sunway.edu.my>
2023-07-24 16:41:50 +0800

    just testing

--- src/py_mrjob/tmp.py
+++ src/py_mrjob/tmp.py
@@ -1,10 +1,19 @@
-import subprocess
-cat = subprocess.Popen(["hadoop", "fs", "-cat", "/user/hadoop/results/py_mapred_streaming/job2/part-00000"], stdout=subprocess.PIPE)
-cnt = 1
-for line in cat.stdout:
-    
-    if cnt > 10:
-        break
-        
-    line = line.strip().split('\t',1)
-    print(line[0])
+#!/usr/bin/env python3
+
+from mrjob.job import MRJob
+
+class DataDividedByMovie(MRJob):      
+
+    def mapper(self, _, line):
+        line = line.strip().split("\t", 2)
+
+        yield line[0], line[1]
+
+    def reducer(self, movie, values):
+        yield movie, list(values)
+
+    # def reducer(self, key, values):
+    #     yield(key, list(values))
+
+if __name__ == '__main__':
+    DataDividedByMovie.run()
