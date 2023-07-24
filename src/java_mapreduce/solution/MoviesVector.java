a615e36b7500cfd0e62aa0197b7d694419775f9c
Grg0rry <19084193@imail.sunway.edu.my>
2023-07-24 23:14:34 +0800

    updated MoviesVector java

--- src/java_mapreduce/solution/MoviesVector.java
+++ src/java_mapreduce/solution/MoviesVector.java
@@ -10,8 +10,8 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
