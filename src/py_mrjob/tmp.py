05be44cd27c6e3ca96523ff377fcce225e47aa6a
Grg0rry <19084193@imail.sunway.edu.my>
2023-07-24 16:56:53 +0800

    just testing

--- src/py_mrjob/tmp.py
+++ src/py_mrjob/tmp.py
@@ -8,7 +8,7 @@ class DataDividedByMovie(MRJob):
         line = line.strip().split("\t", 1)
 
         MovieTitle = str(line[0])
-        UserID, Rating = str(line[1]).split(':')
+        UserID, Rating = str(line[1]).strip().split(':')
 
         yield UserID, int(Rating)
 
