f508f2cbf8816e7de3cade88d139ce0b1f52fe0c
Grg0rry <19084193@imail.sunway.edu.my>
2023-07-29 00:12:03 +0800

    Updated java code

--- src/java_mapreduce/solution/CosineSimilarity.java
+++ src/java_mapreduce/solution/CosineSimilarity.java
@@ -16,11 +16,9 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.util.Tool;
-import org.apache.hadoop.util.ToolRunner;
 
 
-public class CosineSimilarity extends Configured implements Tool {
+public class CosineSimilarity {
 
     /* Mapper */
     public static class CosineSimilarityMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
