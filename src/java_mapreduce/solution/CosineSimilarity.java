fb99760987dfa5857cb31e5b6444a190fc0e8efe
Grg0rry <19084193@imail.sunway.edu.my>
2023-07-28 22:13:31 +0800

    Updated java code

--- src/java_mapreduce/solution/CosineSimilarity.java
+++ src/java_mapreduce/solution/CosineSimilarity.java
@@ -3,10 +3,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -16,73 +18,63 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+
+
 public class CosineSimilarity extends Configured implements Tool {
 
     /* Mapper */
-    public static class CosineSimilarityMapper extends Mapper<Object, Text, Text, Text> {
+    public static class CosineSimilarityMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 
         @Override
-        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            String[] line = value.toString().split("\t", 2);
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            
+            String[] line = value.toString().trim().split("\t", 2);
+
             String movieTitle = line[0];
-            String[] ratingsArray = line[1].substring(1, line[1].length() - 1).split(", ");
-            List<Integer> vector = new ArrayList<>();
+            String[] ratingsArray = line[1].split(",");
 
             for (String rating : ratingsArray) {
-                vector.add(Integer.parseInt(rating));
+                context.write(new Text(movieTitle), new IntWritable(Integer.parseInt(rating)));
             }
-
-            context.write(new Text(movieTitle), new Text(vector.toString()));
         }
     }
 
     /* Reducer */
-    public static class CosineSimilarityReducer extends Reducer<Text, Text, Text, DoubleWritable> {
+    public static class CosineSimilarityReducer extends Reducer<Text, IntWritable, Text, DoubleWritable> {
 
         private Map<String, List<Integer>> movieVectorMap = new HashMap<>();
         private Map<String, Double> magnitudeMap = new HashMap<>();
 
         @Override
-        protected void reduce(Text key, Iterable<Text> values, Context context)
-                throws IOException, InterruptedException {
+        protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
             List<Integer> movieVector = new ArrayList<>();
 
-            for (Text value : values) {
-                String[] vectorStr = value.toString().substring(1, value.toString().length() - 1).split(",");
-                for (String str : vectorStr) {
-                    movieVector.add(Integer.parseInt(str.trim()));
-                }
+            for (IntWritable value: values){
+                movieVector.add(value.get());
             }
+            movieVectorMap.put(key, movieVector);
 
-            movieVectorMap.put(key.toString(), movieVector);
-
-            double magnitude = 0;
-            for (int val : movieVector) {
-                magnitude += val * val;
-            }
-            magnitude = Math.sqrt(magnitude);
-            magnitudeMap.put(key.toString(), magnitude);
+            double magnitude = Nd4j.create(movieVector).norm2Number().doubleValue();
+            magnitudeMap.put(key, magnitude);
         }
 
         @Override
         protected void cleanup(Context context) throws IOException, InterruptedException {
             for (Map.Entry<String, List<Integer>> entry1 : movieVectorMap.entrySet()) {
                 String movieTitle1 = entry1.getKey();
-                List<Integer> vector1 = entry1.getValue();
+                INDArray vector1 = Nd4j.create(entry1.getValue());
                 double magnitude1 = magnitudeMap.get(movieTitle1);
-
+                
                 for (Map.Entry<String, List<Integer>> entry2 : movieVectorMap.entrySet()) {
                     String movieTitle2 = entry2.getKey();
-                    List<Integer> vector2 = entry2.getValue();
+                    INDArray vector2 = Nd4j.create(entry2.getValue());
                     double magnitude2 = magnitudeMap.get(movieTitle2);
-
-                    double dotProduct = 0;
-                    for (int i = 0; i < vector1.size(); i++) {
-                        dotProduct += vector1.get(i) * vector2.get(i);
-                    }
-
+                    
+                    double dotProduct = vector1.mmul(vector2.transpose()).getDouble(0, 0);
                     double similarity = dotProduct / (magnitude1 * magnitude2);
-                    context.write(new Text(movieTitle1 + " - " + movieTitle2), new DoubleWritable(similarity));
+                    context.write(new Text(movieTitle1.toString()+","+movieTitle2.toString()), new DoubleWritable(similarity));
                 }
             }
         }
