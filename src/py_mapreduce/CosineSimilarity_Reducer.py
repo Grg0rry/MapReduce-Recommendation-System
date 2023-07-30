f03a4dac39ad444c040ef952ed9efe61452365b2
Grg0rry <19084193@imail.sunway.edu.my>
2023-07-30 21:04:37 +0800

    updated src code

--- src/py_mapreduce/CosineSimilarity_Reducer.py
+++ src/py_mapreduce/CosineSimilarity_Reducer.py
@@ -2,42 +2,50 @@
 
 import sys
 from itertools import combinations
-# import numpy as np
+import numpy as np
 
-Movie_Vector = {}
-Magnitude = {}
+# Movie_Vector = {}
+# Magnitude = {}
 
-for line in sys.stdin:
-    line = line.strip().split('\t', 1)
+# for line in sys.stdin:
+#     line = line.strip().split('\t', 1)
 
-    MovieTitle = line[0]
-    Vector = [int(item.strip()) for item in line[1].strip('[]').split(',')]
+#     MovieTitle = line[0]
+#     Vector = [int(item.strip()) for item in line[1].strip('[]').split(',')]
     
-    # Vector = np.array([int(item.strip()) for item in line[1].strip('[]').split(',')])
-    # Magnitude[MovieTitle] = np.linalg.norm(Vector)
-    
-    Movie_Vector[MovieTitle] = Vector
-    Magnitude[MovieTitle] = sum(component ** 2 for component in Vector) ** 0.5
+#     Vector = np.array([int(item.strip()) for item in line[1].strip('[]').split(',')])
+#     Magnitude[MovieTitle] = np.linalg.norm(Vector)
+#     Movie_Vector[MovieTitle] = Vector
 
-
-combinations_set = set()
-for (MovieTitle_1, Vector_1), (MovieTitle_2, Vector_2) in combinations(Movie_Vector.items(), 2):
-    if frozenset([MovieTitle_1, MovieTitle_2]) not in combinations_set and MovieTitle_1 != MovieTitle_2:
-        dot_product = sum(v1 * v2 for v1, v2 in zip(Vector_1, Vector_2))
-        similarity = dot_product / (Magnitude[MovieTitle_1] * Magnitude[MovieTitle_2])
-        print('%s\t%s' % ((MovieTitle_1, MovieTitle_2), similarity))
-        print('%s\t%s' % ((MovieTitle_2, MovieTitle_1), similarity))
-        combinations_set.add(frozenset([MovieTitle_1, MovieTitle_2]))
-
-
-# for MovieTitle_1, Vector_1 in Movie_Vector.items():
-#     for MovieTitle_2, Vector_2 in Movie_Vector.items():
-#         if MovieTitle_1 != MovieTitle_2:
-#             dot_product = sum(v1 * v2 for v1, v2 in zip(Vector_1, Vector_2))
-#             similarity = dot_product / (Magnitude[MovieTitle_1] * Magnitude[MovieTitle_2])
-#             print('%s\t%s' % ((MovieTitle_1, MovieTitle_2), similarity))
 
 # for (MovieTitle, Vector), (Next_MovieTitle, Next_Vector) in combinations(Movie_Vector.items(), 2):
 #     dot_product = np.dot(Vector, Next_Vector)
 #     similarity = dot_product / (Magnitude[MovieTitle] * Magnitude[Next_MovieTitle])
 #     print('%s\t%s' % ((MovieTitle, Next_MovieTitle), similarity))
+#     print('%s\t%s' % ((Next_MovieTitle, MovieTitle), similarity))
+
+
+movie_vector_map = {}
+magnitude_map = {}
+combinations_set = set()
+
+for line in sys.stdin:
+    line = line.strip().split('\t', 1)
+
+    MovieTitle = line[0]
+    Vector = np.array([int(item.strip()) for item in line[1].strip('[]').split(',')])
+    magnitude_map[MovieTitle] = np.linalg.norm(Vector)
+    movie_vector_map[MovieTitle] = Vector
+
+
+similarity_map = {}
+for movie_title1, vector1 in movie_vector_map.items():
+    for movie_title2, vector2 in movie_vector_map.items():
+        if movie_title1 != movie_title2 and (movie_title1 + "," + movie_title2) not in combinations_set:
+            dot_product = np.dot(vector1, vector2)
+            
+            similarity = dot_product / (magnitude_map[movie_title1] * magnitude_map[movie_title2])
+            print('%s\t%s' % ((movie_title1, movie_title2), similarity))
+            
+            combinations_set.add(movie_title1 + "," + movie_title2)
+            combinations_set.add(movie_title2 + "," + movie_title1)
